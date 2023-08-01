2fafb28bc54fd1142dd54293e28a9e67a0c6f536
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2023-08-01 21:10:03 +0530

    Updated Solution - GfG to GitHub

--- Basic/Print first n Fibonacci Numbers/print-first-n-fibonacci-numbers.cpp
+++ Basic/Print first n Fibonacci Numbers/print-first-n-fibonacci-numbers.cpp
@@ -10,22 +10,30 @@ using namespace std;
 
 class Solution
 {
-    public:
-    //Function to return list containing first n fibonacci numbers.
+private:
+    long long f(int n, vector<long long>& dp) {
+        if (n <= 1)
+            return 1; // Changed to return 1 instead of n for Fibonacci sequence to start with 1 1.
+        if (dp[n] != -1)
+            return dp[n];
+        return dp[n] = f(n - 1, dp) + f(n - 2, dp);
+    }
 
-    vector<long long> printFibb(int n) 
+public:
+    // Function to return list containing first n fibonacci numbers.
+    vector<long long> printFibb(int n)
     {
-        vector<long long>dp(n,-1);
-        dp[0] =1;
-        dp[1] =1;
-        
-        for(int i=2;i<n;i++){
-            dp[i] = dp[i-1] +dp[i-2];
+        vector<long long> dp(n, -1);
+        dp[0] = dp[1] = 1; // Initialize the first two elements as 1.
+        for (int i = 2; i < n; i++) {
+            dp[i] = f(i, dp); // Calculate the Fibonacci numbers starting from the third element.
         }
-        
-    return dp; 
+        return dp;
     }
 };
+
+
+
 
 //{ Driver Code Starts.
 int main()
