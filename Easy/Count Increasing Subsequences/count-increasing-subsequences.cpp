85f5a9af48afa45ab2101234c6274e76779f538a
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2023-12-12 09:11:07 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count Increasing Subsequences/count-increasing-subsequences.cpp
+++ Easy/Count Increasing Subsequences/count-increasing-subsequences.cpp
@@ -7,20 +7,38 @@ using namespace std;
 
 class Solution{
     public:
+    // Function To Count all the sub-sequences
+    // possible in which digit is greater than
+    // all previous digits arr[] is array of n
+    // digits
     unsigned long long int countSub(int arr[], int n)
     {
-        // Your code goes here
-        unsigned long long ans = 0;
-        vector<double>dp(n,1);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(arr[i]> arr[j]){
-                    dp[i] += dp[j];
-                }
-            }
-            ans+= dp[i];
+        // count[] array is used to store all sub-
+        // sequences possible using that digit
+        // count[] array covers all the digit
+        // from 0 to 9
+        unsigned long long int count[10] = {0};
+    
+        // scan each digit in arr[]
+        for (int i=0; i<n; i++)
+        {
+            // count all possible sub-sequences by
+            // the digits less than arr[i] digit
+            for (int j=arr[i]-1; j>=0; j--)
+                count[arr[i]] += count[j];
+    
+            // store sum of all sub-sequences plus
+            // 1 in count[] array
+            count[arr[i]]++;
         }
-        return (unsigned long long)ans;
+    
+        // now sum up the all sequences possible in
+        // count[] array
+        unsigned long long int result = 0;
+        for (unsigned long long int i=0; i<10; i++)
+            result += count[i];
+    
+        return result;
     }
 };
 
