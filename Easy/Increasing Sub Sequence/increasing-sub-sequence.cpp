8393a32dfda606a3377dde16d216820276148629
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2023-12-18 15:14:47 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Increasing Sub Sequence/increasing-sub-sequence.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+public:
+// Function to find the length of the longest increasing subsequence
+int longestIncreasingSubsequence(int arr[], int n) {
+    // Create a temporary vector to store the increasing subsequence
+    vector<int> temp;
+    temp.push_back(arr[0]);
+
+    int len = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > temp.back()) {
+            // If arr[i] is greater than the last element of temp, extend the subsequence
+            temp.push_back(arr[i]);
+            len++;
+        } else {
+            // If arr[i] is not greater, replace the element in temp with arr[i]
+            int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+            temp[ind] = arr[i];
+        }
+    }
+
+    return len;
+}
+    int maxLength(string S)
+    {
+        // code here
+        int n = S.length();
+        int arr[n+1] = {0};
+        for(int i=0; i<n; i++){
+            arr[i] = S[i]-'0';
+        }
+        
+        return longestIncreasingSubsequence(arr,n);
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin>>S;
+        Solution ob;
+        cout << ob.maxLength(S) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
