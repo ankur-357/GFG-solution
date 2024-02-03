2a3592b5016d6df43a337ca86b886ed609e41656
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2024-02-03 19:29:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Duplicate subtree in Binary Tree/duplicate-subtree-in-binary-tree.cpp
@@ -0,0 +1,141 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	char data;
+	struct Node *left;
+	struct Node *right;
+
+	Node(char x) {
+		data = x;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+struct Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node *root = new Node(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current Node
+			currNode->left = new Node(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = new Node(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+
+// } Driver Code Ends
+/*The structure of the Binary Tree Node  is
+struct Node
+{
+  char data;
+  struct Node* left;
+  struct Node* right;
+};*/
+
+class Solution {
+public:
+    string serialize(Node* root, unordered_map<string, int>& subtreeCount, unordered_map<string, Node*>& subtreeRoots) {
+        if (!root)
+            return "#";
+
+        string serialized = to_string(root->data) + "," + serialize(root->left, subtreeCount, subtreeRoots) + "," + serialize(root->right, subtreeCount, subtreeRoots);
+
+        if (++subtreeCount[serialized] == 2) {
+            subtreeRoots[serialized] = root;
+        }
+
+        return serialized;
+    }
+
+    bool dupSub(Node* root) {
+        unordered_map<string, int> subtreeCount;
+        unordered_map<string, Node*> subtreeRoots;
+
+        serialize(root, subtreeCount, subtreeRoots);
+
+        for (const auto& entry : subtreeRoots) {
+            if (entry.second->left || entry.second->right) {
+                return true; // Found a duplicate subtree of size two or more
+            }
+        }
+
+        return false;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+	
+	int t;
+	cin >> t;
+	//cout << t << "\n";
+	while (t--)
+	{
+		string treeString;
+		getline(cin >> ws, treeString);
+		struct Node* root = buildTree(treeString);
+		Solution ob;
+		cout << ob.dupSub(root) << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
